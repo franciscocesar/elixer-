cc910370eda98d5855434584b5ba21024ac62107
franciscocesar <francisco.mateus@souunit.com.br>
2022-11-11 15:24:07 -0300

    feat: add models in backoffice

---
+++ src/backoffice/models/customer.model.ts
@@ -0,0 +1,17 @@
+import { Address } from './address.model';
+import { CreditCard } from './credit-card.model';
+import { Pet } from './pet.model';
+
+export class Customer {
+  constructor(
+    public name: string,
+    public document: string,
+    public email: string,
+    public password: string,
+    public billingAddress: Address,
+    public shippingAddress: Address,
+    public creditCard: CreditCard,
+    public pets: Pet[],
+    public active: boolean,
+  ) {}
+}
