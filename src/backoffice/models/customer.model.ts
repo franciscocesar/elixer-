c1fdc01d57f12f3901d47dc02c86b083f08ab0b9
franciscocesar <francisco.mateus@souunit.com.br>
2022-11-14 17:38:10 -0300

    fix: adjust customer types constructor

--- src/backoffice/models/customer.model.ts
+++ src/backoffice/models/customer.model.ts
@@ -1,17 +1,17 @@
 import { Address } from './address.model';
 import { CreditCard } from './credit-card.model';
 import { Pet } from './pet.model';
+import { User } from './user.model';
 
 export class Customer {
   constructor(
     public name: string,
     public document: string,
     public email: string,
-    public password: string,
     public billingAddress: Address,
     public shippingAddress: Address,
     public creditCard: CreditCard,
     public pets: Pet[],
-    public active: boolean,
+    public user: User,
   ) {}
 }
