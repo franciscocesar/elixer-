376e1b748a15e39aeb3ff2d274312aabb8af058b
franciscocesar <francisco.mateus@souunit.com.br>
2022-11-14 17:35:44 -0300

    feat: create moongoose module for root

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BackofficeModule } from './backoffice/backoffice.module';
 
 @Module({
-  imports: [BackofficeModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://franciscocsar:chico_hd2014@database.geztvc1.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    BackofficeModule,
+  ],
   controllers: [],
   providers: [],
 })
