144b9525583d78fec94c2195f14040c56cd5bfc4
franciscocesar <francisco.mateus@souunit.com.br>
2022-11-14 17:38:30 -0300

    feat: create customer schema

---
+++ src/backoffice/schemas/customer.schema.ts
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose';
+
+export const CustomerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  document: {
+    type: String,
+    required: true,
+    trim: true,
+    index: {
+      unique: true,
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    index: {
+      unique: true,
+    },
+  },
+  pets: [
+    {
+      name: {
+        type: String,
+      },
+      gender: {
+        type: String,
+        enum: ['male', 'female', 'none'],
+      },
+      kind: {
+        type: String,
+      },
+      brand: {
+        type: String,
+      },
+    },
+  ],
+  billingAddress: {
+    zipcode: {
+      type: String,
+    },
+    street: {
+      type: String,
+    },
+    number: {
+      type: String,
+    },
+    complement: {
+      type: String,
+    },
+    neighborhood: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+  },
+  card: {
+    number: {
+      type: String,
+    },
+    holder: {
+      type: String,
+    },
+    expiration: {
+      type: String,
+    },
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    require: true,
+  },
+});
