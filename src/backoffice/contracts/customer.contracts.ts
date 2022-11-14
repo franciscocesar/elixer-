71be0a8647c33cdfd965f405bca340454c7562a2
franciscocesar <francisco.mateus@souunit.com.br>
2022-11-14 17:36:51 -0300

    fix: adjust contract validator

--- src/backoffice/contracts/customer.contracts.ts
+++ src/backoffice/contracts/customer.contracts.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Flunt } from 'src/utils/flunt';
-import { Customer } from '../models/customer.model';
+import { CreateCustomerDTO } from '../dtos/create-customer-dto';
 import { Contract } from './contract';
 
 @Injectable()
 export class CreateCustomerContract implements Contract {
   errors: any[];
-  validate(model: Customer): boolean {
+  validate(model: CreateCustomerDTO): boolean {
     const flunt = new Flunt();
     flunt.hasMinLen(model.name, 5, 'Nome inválido');
     flunt.isEmail(model.email, 'E-mail inválido');
     flunt.isFixedLen(model.document, 11, 'CPF inválido');
-    flunt.hasMinLen(model.password, 6, 'Senha Inválida');
+    flunt.hasMinLen(model.password, 6, 'Senha inválida');
 
     this.errors = flunt.errors;
     return flunt.isValid();
