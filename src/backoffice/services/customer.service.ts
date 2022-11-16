d383768b8b98981eb2411b5fe78980237ac3c9ec
franciscocesar <francisco.mateus@souunit.com.br>
2022-11-15 23:49:32 -0300

    feat: add address service

--- src/backoffice/services/customer.service.ts
+++ src/backoffice/services/customer.service.ts
@@ -1,14 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Address } from '../models/address.model';
+import { Customer } from '../models/customer.model';
+import { Pet } from '../models/pet.model';
 import { User } from '../models/user.model';
 
 @Injectable()
 export class CustomerService {
-  constructor(@InjectModel('Customer') private readonly model: Model<User>) {}
+  constructor(@InjectModel('Customer') private readonly model: Model<User>) { }
 
-  async create(data: User): Promise<User> {
+  async create(data: Customer): Promise<User> {
     const customer = new this.model(data);
     return await customer.save();
+  }
+
+  async addBillingAddress(document: string, data: Address): Promise<Customer> {
+    const options = { upsert: true }
+    return await this.model.findOneAndUpdate({ document }, {
+      $set: {
+        billingAddress: data,
+      }
+    }, options)
+  }
+
+  async addShippingAddress(document: string, data: Address): Promise<Customer> {
+    const options = { upsert: true }
+    return await this.model.findOneAndUpdate({ document }, {
+      $set: {
+        shippingAddress: data,
+      }
+    }, options)
+  }
+
+  async createPet(document: string, data: Pet): Promise<Customer> {
+    const options = { upsert: true, new: true }
+    return await this.model.findOneAndUpdate({ document }, {
+      $push: {
+        shippingAddress: data,
+      }
+    }, options)
   }
 }
