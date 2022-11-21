1ed10bc2af6a7d16bbf7a98e637d433d2587eac6
franciscocesar <francisco.mateus@souunit.com.br>
2022-11-21 17:37:45 -0300

    feat: create Store module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
+import { StoreModule } from './store/store.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://franciscocsar:chico_hd2014@database.geztvc1.mongodb.net/?retryWrites=true&w=majority',
     ),
     BackofficeModule,
+    StoreModule,
   ],
   controllers: [],
   providers: [],
