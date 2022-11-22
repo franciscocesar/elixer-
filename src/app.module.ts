e61dd32ed2ec14a7b245c520275d5e01f864c446
franciscocesar <francisco.mateus@souunit.com.br>
2022-11-22 03:12:53 -0300

    chore: create conections on database mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
+
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { databaseProviders } from './database/database.provider';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
-import { StoreModule } from './store/store.module';
+import { StoreModule } from './modules/store/store.module';
+
+
 @Module({
   imports: [
     MongooseModule.forRoot(
@@ -11,6 +15,7 @@ import { StoreModule } from './store/store.module';
     StoreModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [...databaseProviders],
+  exports: [...databaseProviders]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/database/database.provider.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+    {
+        provide: 'DATA_SOURCE',
+        useFactory: async () => {
+            const dataSource = new DataSource({
+                type: 'mysql',
+                host: 'localhost',
+                port: 3306,
+                username: 'root',
+                password: 'Chico_hd2014',
+                database: 'petshop',
+                entities: [
+                    __dirname + '/../**/*.entity{.ts,.js}',
+                ],
+                synchronize: true,
+            });
+
+            return dataSource.initialize();
+        },
+    },
+];
